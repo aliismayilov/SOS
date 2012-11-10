b60eefb3ac4a21a66b4562eb1cc5b2953df2a79f
Ali Ismayilov <ali@ismailov.info>
2012-11-10 23:51:43 +0400

    page/entry admin autogenerate slug field

--- cms/admin.py
+++ cms/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 
 from cms.models import *
 
+class PageAdmin(admin.ModelAdmin):
+	prepopulated_fields = {"slug": ("title",)}
+
+class EntryAdmin(admin.ModelAdmin):
+	prepopulated_fields = {"slug": ("title",)}
+
 admin.site.register(Language)
-admin.site.register(Page)
-admin.site.register(Entry)
+admin.site.register(Page, PageAdmin)
+admin.site.register(Entry, EntryAdmin)
