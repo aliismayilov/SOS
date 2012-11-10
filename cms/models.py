37fa840bfeee3c85105924c84444a2df1871767f
Ali Ismayilov <ali@ismayilov.info>
2012-11-10 15:55:17 +0400

    create models

--- cms/models.py
+++ cms/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Language(models.Model):
+	small_name = models.CharField(max_length=3)
+	long_name = models.CharField(max_length=20)
+
+	def __unicode__(self):
+		return self.small_name
+
+class Page(models.Model):
+	language = models.ForeignKey('Language')
+	
+	title = models.CharField(max_length=50)
+	slug = models.SlugField()
+	body = models.TextField()
+
+	def __unicode__(self):
+		return self.title
+
+class Entry(models.Model):
+	language = models.ForeignKey('Language')
+
+	title = models.CharField(max_length=50)
+	slug = models.SlugField()
+	body = models.TextField()
+
+	date_published = models.DateTimeField()
+
+	def __unicode__(self):
+		return self.title
