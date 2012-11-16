c511d8d0237770feff88025e32343df339e4fe33
Ali Ismayilov <ali@ismailov.info>
2012-11-16 22:39:49 +0400

    show_page.html and show_page view

--- cms/views.py
+++ cms/views.py
@@ -1,4 +1,6 @@
-from cms.models import Language, Page
+from datetime import datetime
+
+from cms.models import Language, Page, Entry
 
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render_to_response
@@ -12,7 +14,16 @@ def show_page(request, language, slug):
 
 	return render_to_response('show_page.html', {'page': page})
 
-def show_entry(request, language, year, month, day, slug):
-	return HttpResponse('language: %s, year: %s, month: %s, day: %s, slug: %s' % (
-			language, year, month, day, slug
-		))
+def show_entry(request, language, date, slug):
+	language = get_object_or_404(Language, small_name=language)
+	date = datetime.strptime(date, "%Y/%b/%d")
+
+	entry = get_object_or_404(Entry,
+			language=language,
+			date_published__year=date.year,
+			date_published__month=date.month,
+			date_published__day=date.day,
+			slug=slug
+		)
+
+	return render_to_response('show_entry.html', {'entry': entry})
