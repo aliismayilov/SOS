8b07646adb23b9cf9c5099250679b88b6e280feb
Ali Ismayilov <ali@ismailov.info>
2012-11-27 08:35:26 +0400

    corrected index url

--- SOS/urls.py
+++ SOS/urls.py
@@ -11,5 +11,5 @@ urlpatterns += patterns('cms.views',
 	url(r'^(?P<language>[a-z]{2,3})/(?P<date>\d{4}/[a-z]{3}/\d{2})/(?P<slug>(\w|-)+).html$', 'show_entry'),
 	url(r'^(?P<language>[a-z]{2,3})/(?P<slug>(\w|-)+).html$', 'show_page'),
 	url(r'^(?P<language>[a-z]{2,3})/$', 'index'),
-	url(r'', 'index'),
+	url(r'^$', 'index'),
 )
