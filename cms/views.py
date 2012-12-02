059ef0a2f3f80f080e5bad785a4bbeb8f2204ed4
Ali Ismayilov <ali@ismailov.info>
2012-12-02 22:02:34 +0400

    moved homepage to index.html, all basic templates are working

--- cms/views.py
+++ cms/views.py
@@ -9,7 +9,7 @@ from django.template import RequestContext
 def index(request, language='en'):
 	language = get_object_or_404(Language, small_name=language)
 
-	return render_to_response('base.html', {
+	return render_to_response('index.html', {
 				'current_language': language,
 				'languages': Language.objects.all(),
 				'top_links': Page.objects.filter(language=language, parent=None)[:5],
@@ -23,7 +23,16 @@ def show_page(request, language, slug):
 	language = get_object_or_404(Language, small_name=language)
 	page = get_object_or_404(Page, slug=slug, language=language)
 
-	return render_to_response('show_page.html', {'page': page})
+	return render_to_response('show_page.html', {
+				'current_language': language,
+				'languages': Language.objects.all(),
+				'top_links': Page.objects.filter(language=language, parent=None)[:5],
+				'bottom_links': Page.objects.filter(language=language, parent=None)[5:8],
+				'side_links': Page.objects.filter(language=language, parent=None)[8:],
+				'latest_entries': Entry.objects.filter(language=language).exclude(image=None)[:3],
+				'page': page,
+			},
+		context_instance=RequestContext(request))
 
 def show_entry(request, language, date, slug):
 	language = get_object_or_404(Language, small_name=language)
@@ -37,4 +46,13 @@ def show_entry(request, language, date, slug):
 			slug=slug
 		)
 
-	return render_to_response('show_entry.html', {'entry': entry})
+	return render_to_response('show_entry.html', {
+				'current_language': language,
+				'languages': Language.objects.all(),
+				'top_links': Page.objects.filter(language=language, parent=None)[:5],
+				'bottom_links': Page.objects.filter(language=language, parent=None)[5:8],
+				'side_links': Page.objects.filter(language=language, parent=None)[8:],
+				'latest_entries': Entry.objects.filter(language=language).exclude(image=None)[:3],
+				'entry': entry,
+			},
+		context_instance=RequestContext(request))
