fd32405eedf9466104ca2d81c1334cc06d6381ca
Ali Ismayilov <ali@ismailov.info>
2012-12-09 13:22:14 +0400

    archive ready

--- cms/views.py
+++ cms/views.py
@@ -57,22 +57,30 @@ def show_entry(request, language, date, slug):
 			},
 		context_instance=RequestContext(request))
 
-def archive(request, language, year, month=None):
+def archive(request, language, year=None, month=None):
 	language = get_object_or_404(Language, small_name=language)
 	
-	if month:
+	if year and month:
 		date = datetime.strptime(year + "/" + month, "%Y/%b")
 		entries = Entry.objects.filter(
 				language=language,
 				date_published__year=date.year,
 				date_published__month=date.month,
 			)
-	else:
+		year_range = None
+	elif year:
 		date = datetime.strptime(year, "%Y")
 		entries = Entry.objects.filter(
 				language=language,
 				date_published__year=date.year,
 			)
+		year_range = None
+	else:
+		first_date = Entry.objects.all().reverse()[0].date_published
+		last_date = Entry.objects.all()[0].date_published
+		year_range = range(first_date.year, last_date.year + 1)
+		entries = []
+		date = datetime.now()
 
 	return render_to_response('archive.html', {
 				'current_language': language,
@@ -83,5 +91,6 @@ def archive(request, language, year, month=None):
 				'entries': entries,
 				'year': date.year,
 				'month': date.strftime("%B") if month else '',
+				'year_range': year_range,
 			},
 		context_instance=RequestContext(request))
