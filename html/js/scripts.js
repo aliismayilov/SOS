feff9c3ef2e91ea7976af598da0cafc9a4cc7c98
Ali Ismayilov <ali@ismailov.info>
2012-12-25 15:25:01 +0400

    html cutting improvement

--- html/js/scripts.js
+++ html/js/scripts.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
 	$('.carousel').carousel();
+	$(".tweet").tweet({
+          avatar_size: 32,
+          count: 4,
+          username: "sos_children",
+          template: "{text} » {retweet_action}"
+        });
+      }).bind("loaded", function(){
+        $(this).find("a.tweet_action").click(function(ev) {
+          window.open(this.href, "Retweet",
+                      'menubar=0,resizable=0,width=550,height=420,top=200,left=400');
+          ev.preventDefault();
+        });
 });
