1d600f2b5f0307657ad98dc1c2f9ff09838b3965
Ali Ismayilov <ali@ismailov.info>
2012-12-26 10:58:28 +0400

    html cutting improvement

--- html/js/scripts.js
+++ html/js/scripts.js
@@ -1,15 +1,39 @@
 $(document).ready(function() {
 	$('.carousel').carousel();
+	
 	$(".tweet").tweet({
-          avatar_size: 32,
-          count: 4,
-          username: "sos_children",
-          template: "{text} » {retweet_action}"
-        });
-      }).bind("loaded", function(){
-        $(this).find("a.tweet_action").click(function(ev) {
-          window.open(this.href, "Retweet",
-                      'menubar=0,resizable=0,width=550,height=420,top=200,left=400');
-          ev.preventDefault();
-        });
+		avatar_size: 32,
+		count: 1,
+		username: "sos_children",
+		template: "{text}"
+	});
+
+	$('#shareme').sharrre({
+		share: {
+			twitter: true,
+			facebook: true,
+			googlePlus: true
+		},
+		template: '<div class="box"><div class="left">Share</div><div class="middle"><a href="#" class="facebook">f</a><a href="#" class="twitter">t</a><a href="#" class="googleplus">+1</a></div><div class="right">{total}</div></div>',
+		enableHover: false,
+		enableTracking: true,
+		render: function (api, options) {
+			$(api.element).on('click', '.twitter', function () {
+				api.openPopup('twitter');
+			});
+			$(api.element).on('click', '.facebook', function () {
+				api.openPopup('facebook');
+			});
+			$(api.element).on('click', '.googleplus', function () {
+				api.openPopup('googlePlus');
+			});
+		}
+	});
+
+
+}).bind("loaded", function () {
+	$(this).find("a.tweet_action").click(function(ev) {
+		window.open(this.href, "Retweet", 'menubar=0,resizable=0,width=550,height=420,top=200,left=400');
+		ev.preventDefault();
+	});
 });
