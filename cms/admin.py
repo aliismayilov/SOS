8b53a971793f29f12f8a16c393e843426122223a
Ali Ismayilov <ali@ismailov.info>
2013-01-21 20:48:53 +0400

    admin language filter

--- cms/admin.py
+++ cms/admin.py
@@ -5,12 +5,14 @@ from cms.models import *
 
 class PageAdmin(admin.ModelAdmin):
 	prepopulated_fields = {"slug": ("title",)}
+	list_filter = ('language',)
 	class Media:
 		js = (settings.STATIC_URL + 'tiny_mce/tiny_mce.js',
 			  settings.STATIC_URL + 'js/tiny_mce_init.js',)
 
 class EntryAdmin(admin.ModelAdmin):
 	prepopulated_fields = {"slug": ("title",)}
+	list_filter = ('language',)
 	class Media:
 		js = (settings.STATIC_URL + 'tiny_mce/tiny_mce.js',
 			  settings.STATIC_URL + 'js/tiny_mce_init.js',)
